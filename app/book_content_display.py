c7b69cc1f684e26b809eacb3d3372f3641f17870
Danylo-D87 <quaestor88@gmail.com>
2025-06-15 18:26:17 +0300

    improve potential imports problem

--- app/book_content_display.py
+++ app/book_content_display.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from book import Book
+from app.book import Book
 
 
 class BookContentDisplay(ABC):
--- app/book_printer.py
+++ app/book_printer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from book import Book
+from app.book import Book
 
 
 class BookPrintContentDisplay(ABC):
